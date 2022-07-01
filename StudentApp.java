d7526b985e9a7b1516946d17511aa3f13a5d5cbd
ganeshvaayuh <108187945+ganeshvaayuh@users.noreply.github.com>
2022-07-01 10:45:52 +0530

    Updated Student App

--- StudentApp.java
+++ StudentApp.java
@@ -3,5 +3,7 @@ class StudentApp
   public static void main(String args[])
   {
     System.out.println("Running student application");
+    System.out.println("Running student application1");
+    
   }
 }
